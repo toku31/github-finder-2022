b91e34d20e1785f44405c3aa9c6260561668ce1f
user <palmtree8880@gmail.com>
2022-11-03 23:57:42 +0900

    key

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -18,7 +18,7 @@ function UserResults() {
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
           // <h3>{user.login}</h3>
-          <UserItem index={user.id} user={user} />
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
